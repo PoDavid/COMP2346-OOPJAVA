ad9b99c25cb4a7b8e4bde64c087e2a339ca42a7f
David <podavid@connect.hku.hk>
2020-03-13 04:56:36 +0800

    Done logic

--- Assignment 3/src/VendingMachine.java
+++ Assignment 3/src/VendingMachine.java
@@ -32,6 +32,33 @@ public class VendingMachine {
     private void clearCoinSlot(){
         coinSlot.clear();
     }
+    private String giveChange(Integer amount){
+        ArrayList<Integer> changesBuffer = new ArrayList<Integer>();
+        Collections.sort(coinChanger);
+        Integer coin;
+        for(int i=coinChanger.size()-1;i>=0;i--){
+            coin = coinChanger.get(i);
+            if (coin<=amount && amount-coin>=0){
+                coinChanger.remove(i);
+                changesBuffer.add(coin);
+                amount-=coin;
+            }
+        }
+        if(amount==0){
+            String response=null;
+            Collections.sort(changesBuffer);
+            for(Integer change:changesBuffer){
+                if(response==null)
+                    response=" $" + change.toString() + ",";
+                else
+                    response+=" $" + change.toString() + ",";
+            }
+            response=response.substring(0, response.length() - 1) + ".";
+            return response;
+        }
+        else
+            return "Insufficient change!";
+    }
     public String rejectCoinFromCoinSlot(){
         String response;
         if (coinSlot.size()==0){
@@ -50,7 +77,6 @@ public class VendingMachine {
     }
     public String purchaseSoftDrinks(String product){
         boolean stock = false;
-        boolean enough_coin = false;
         int slot_index=-1;
         Integer inserted_coin = null;
         Integer price = null;
@@ -76,19 +102,21 @@ public class VendingMachine {
         //Check if needed changes
         if(inserted_coin>price){
             //Check if enough changes
-            if(false){
+            String change_response = giveChange(inserted_coin-price);
+            if (change_response.equals("Insufficient change!"))
                 return "Insufficient change!";
-            }
             else{
-                String response = "Success! Paid $" + price.toString() + ". Change:";
+                String response = "Success! Paid $" + inserted_coin.toString() + ". Change:" + change_response;
+                coinChanger.addAll(coinSlot);
                 clearCoinSlot();
                 return response;
             }
         }
         else{
             softDrinkSlots.get(slot_index).buy();
+            coinChanger.addAll(coinSlot);
             clearCoinSlot();
-            return "Success! Paid $" + price.toString() + ". No change.";
+            return "Success! Paid $" + inserted_coin.toString() + ". No change.";
         }
     }
 }
